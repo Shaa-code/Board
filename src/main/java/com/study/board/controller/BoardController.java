3e50c10321353bc43e58b6889d9a1527954a3f0a
Shaa-code <70310271+Shaa-code@users.noreply.github.com>
2022-08-01 22:47:40 +0900

    Pagenation 하단 페이지 구현

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -4,6 +4,7 @@ import com.study.board.entity.Board;
 import com.study.board.service.BoardService;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -42,8 +43,16 @@ public class BoardController {
 
     @GetMapping("board/lists")
     public String boardList(Model model,@PageableDefault(page = 0, size = 10, sort = "id" , direction = Sort.Direction.DESC) Pageable pageable) {
+        Page<Board> list = boardService.boardList(pageable);
 
-        model.addAttribute("list", boardService.boardList(pageable));
+        int nowPage = list.getPageable().getPageNumber()+1; // list.getPageble은 pageable로 치환가능.
+        int startPage = Math.max(nowPage - 4,1); // max로 1 미만으로 못내려가게 만드는 로직
+        int endPage = Math.min(nowPage + 5, list.getTotalPages()); // 최대값 이상이면 오류가 나므로, 넘어가면 0을 반환하도록 설정!
+
+        model.addAttribute("list", list);
+        model.addAttribute("nowPage",nowPage);
+        model.addAttribute("startPage",startPage);
+        model.addAttribute("endPage",endPage);
 
         return "boardlist";
     }
